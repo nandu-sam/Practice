361f676ee4c785039a8bde1d44541b54d18886cc
Nanditha P <nandithanandu57@gmail.com>
2023-12-20 20:14:29 +0530

    modal game

--- model/script.js
+++ model/script.js
@@ -1,1 +1,11 @@
 "use strict";
+const modal = document.querySelector(".modal");
+const overlay = document.querySelector(".overlay");
+const btnCloseModel = document.querySelector(".close-model");
+const btnOpenModel = document.querySelectorAll(".show-model");
+console.log(btnOpenModel);
+for (let i = 0; i < btnOpenModel.length; i++) {
+  btnOpenModel[i].addEventListener("click", function () {
+    console.log("button clicked");
+  });
+}

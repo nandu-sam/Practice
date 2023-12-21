d68b0a4816d7634d2844952fef4acc7bcd8c4193
Nanditha P <nandithanandu57@gmail.com>
2023-12-21 20:03:35 +0530

    model window changes

--- model/script.js
+++ model/script.js
@@ -9,5 +9,18 @@ for (let i = 0; i < btnOpenModal.length; i++) {
     console.log("Button clicked");
     modal.classList.remove("hidden");
     overlay.classList.remove("hidden");
+    //to close the button by using the class
+  });
+
+  btnCloseModal.addEventListener("click", function () {
+    /*these two lines are to close the model window after it is clicked using model add */
+    modal.classList.add("hidden");
+    overlay.classList.add("hidden");
+  });
+
+  /*this is used to click anywhere near overlay (or outside the overlay) to close the model window*/
+  overlay.addEventListener("click", function () {
+    modal.classList.add("hidden");
+    overlay.classList.add("hidden");
   });
 }

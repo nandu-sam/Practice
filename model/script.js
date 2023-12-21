8e2d2f3f664d355206f493fc400d9569ecfbfe06
Nanditha P <nandithanandu57@gmail.com>
2023-12-21 19:31:48 +0530

    changes

--- model/script.js
+++ model/script.js
@@ -1,8 +1,8 @@
 "use strict";
 const modal = document.querySelector(".modal");
 const overlay = document.querySelector(".overlay");
-const btnCloseModel = document.querySelector(".close-model");
-const btnOpenModel = document.querySelectorAll(".show-model");
+const btnCloseModel = document.querySelector(".close-modal");
+const btnOpenModel = document.querySelectorAll(".show-modal");
 console.log(btnOpenModel);
 for (let i = 0; i < btnOpenModel.length; i++) {
   btnOpenModel[i].addEventListener("click", function () {

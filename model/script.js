07dcd1e2d7d8f6b0ca853a948390557b958a4339
Nanditha P <nandithanandu57@gmail.com>
2023-12-21 20:33:50 +0530

    complete modal project

--- model/script.js
+++ model/script.js
@@ -3,19 +3,27 @@ const modal = document.querySelector(".modal");
 const overlay = document.querySelector(".overlay");
 const btnCloseModal = document.querySelector(".close-modal");
 const btnOpenModal = document.querySelectorAll(".show-modal");
-console.log(btnOpenModal);
-for (let i = 0; i < btnOpenModal.length; i++) {
-  btnOpenModal[i].addEventListener("click", function () {
-    console.log("Button clicked");
-    modal.classList.remove("hidden");
-    overlay.classList.remove("hidden");
-    //to close the button by using the class
-  });
 
-  const closeModal = function () {
-    modal.classList.add("hidden");
-    overlay.classList.add("hidden");
-  };
+const openModal = function () {
+  console.log("Button clicked");
+  modal.classList.remove("hidden");
+  overlay.classList.remove("hidden");
+};
+
+const closeModal = function () {
+  modal.classList.add("hidden");
+  overlay.classList.add("hidden");
+};
+
+for (let i = 0; i < btnOpenModal.length; i++) {
+  btnOpenModal[i].addEventListener(
+    "click",
+    openModal
+    //to close the button by using the class
+  );
+  /*key press event (or) esc key press event (or) keyboard event (or) global event because it does not 
+happens on one specific element we usually listen on the whole document*/
+  document.addEventListener("keydown");
 
   btnCloseModal.addEventListener(
     "click",
